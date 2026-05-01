6d6adb7b09d974c03930b83781c32b1393427cd1
Shaurya Chauhan <blues9524@gmail.com>
2026-05-01 14:41:30 +0530

    feat: add mdx support

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
+import createMDX from "@next/mdx"
 import type { NextConfig } from "next"
 import { redirects } from "@/config/redirects"
 
 const nextConfig: NextConfig = {
+  pageExtensions: ["ts", "tsx", "mdx"],
   redirects: async () => redirects
 }
 
-export default nextConfig
+const withMDX = createMDX({})
+
+export default withMDX(nextConfig)
